3f67a1f0e6e945819f6868f45d27b794d031e0e8
Pierre Guyard <pguyard59@gmail.com>
2016-12-04 15:56:34 +0000

    Second commit

--- app/src/main/java/com/example/pierre/mycamera/MainActivity.java
+++ app/src/main/java/com/example/pierre/mycamera/MainActivity.java
@@ -1,14 +1,17 @@
 package com.example.pierre.mycamera;
 
+import android.app.AlertDialog;
 import android.content.ActivityNotFoundException;
 import android.content.ComponentName;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.drawable.BitmapDrawable;
@@ -20,11 +23,13 @@ import android.provider.MediaStore;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -34,25 +39,26 @@ import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class MainActivity extends AppCompatActivity implements View.OnTouchListener{
+public class MainActivity extends AppCompatActivity implements View.OnTouchListener {
 
     //keep track of camera capture intent
-    final int REQUEST_IMAGE_CAPTURE = 1;
-    Uri photoURI; //the Uri for the captured image
-    ImageView mImageView;
-    Canvas canvas;
-    Paint paint;
-    Context mContext;
-    File photoFile;
-    String mCurrentPhotoPath;
-    float downx = 0, downy = 0, upx = 0, upy = 0;
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+    private File photoFile;
+    private Uri photoURI; //the Uri for the captured image
+    private String mCurrentPhotoPath;
+    private ImageView mImageView;
+    private Canvas canvas;
+    private Paint paint;
+    private float downx = 0, downy = 0, upx = 0, upy = 0;
+    private String distanceLine;
+    private EditText editText;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mImageView = (ImageView)findViewById(R.id.picture);
-        // mContext = MainActivity.this;
+        mImageView = (ImageView) findViewById(R.id.picture);
+        editText = (EditText) findViewById(R.id.editText);
     }
 
     @Override
@@ -68,7 +74,7 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        switch(id){
+        switch (id) {
             case R.id.action_settings:
                 return true;
             case R.id.capture_btn:
@@ -82,20 +88,23 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
                         try {
                             photoFile = createImageFile();
                         } catch (IOException ex) {
-
+                            //display an error message
+                            String errorMessage = "Whoops - we cannot create an image file!";
+                            Toast toast = Toast.makeText(this, errorMessage, Toast.LENGTH_SHORT);
+                            toast.show();
                         }
                         // Continue only if the File was successfully created
                         if (photoFile != null) {
                             photoURI = FileProvider.getUriForFile(this, "com.example.pierre.mycamera.fileprovider", photoFile);
                             captureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
-                            //The default Android camera application returns a non-null
-                            // intent only when passing back a thumbnail in the return Intent.
+                            /// This line is essential for this code to work on API < 24.
+                            // This grants permission to the Camera app to write to the specified URI,
+                            // i.e. the folder where pictures are saved for your application.
                             grantUriPermission(cameraApp.getPackageName(), photoURI, Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
                             startActivityForResult(captureIntent, REQUEST_IMAGE_CAPTURE);
                         }
                     }
-                }
-                catch(ActivityNotFoundException anfe){
+                } catch (ActivityNotFoundException anfe) {
                     //display an error message
                     String errorMessage = "Whoops - your device doesn't support capturing images!";
                     Toast toast = Toast.makeText(this, errorMessage, Toast.LENGTH_SHORT);
@@ -108,17 +117,17 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (resultCode == RESULT_OK) {
             //user is returning from capturing an image using the camera
-            if(requestCode == REQUEST_IMAGE_CAPTURE){
+            if (requestCode == REQUEST_IMAGE_CAPTURE) {
                 // Photo has been taken and saved onto storage. Loading it into an ImageView...
                 setPic();
             }
         }
     }
 
-    private File createImageFile() throws IOException {
+    public File createImageFile() throws IOException {
         String timestamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
         String imageFileName = "JPEG_" + timestamp + "_";
         File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
@@ -127,23 +136,7 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
         return image;
     }
 
-    private void displayLayers(Bitmap bmp) {
-        Resources r = getResources();
-        Drawable d = new BitmapDrawable(r,bmp);// converting bitmap to drawable
-        Drawable[] layers = new Drawable[3];
-        layers[0] = d;
-        layers[1] = r.getDrawable(R.drawable.orangeline);
-        // layers[1].setBounds(0,0,20,50);
-        // ColorDrawable leftBorder = new ColorDrawable(Color.RED);
-        TextView tv = new TextView(getApplicationContext());
-        tv.setText("hello");
-        // layers[2] = tv;
-        LayerDrawable layerDrawable = new LayerDrawable(layers);
-        // layerDrawable.setLayerInset(0,0,0,15,0);
-        mImageView.setImageDrawable(layerDrawable);
-    }
-
-    private void setPic() {
+    public void setPic() {
         // Get the dimensions of the View
         int targetW = mImageView.getWidth();
         int targetH = mImageView.getHeight();
@@ -156,7 +149,7 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
         int photoH = bmOptions.outHeight;
 
         // Determine how much to scale down the image
-        int scaleFactor = Math.min(photoW/targetW, photoH/targetH);
+        int scaleFactor = Math.min(photoW / targetW, photoH / targetH);
 
         // Decode the image file into a Bitmap sized to fill the View
         bmOptions.inJustDecodeBounds = false;
@@ -164,11 +157,18 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
         bmOptions.inPurgeable = true;
 
         Bitmap bitmap = BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+        // Resize the Bitmap to have the same dimensions as the ImageView
+        bitmap = getResizedBitmap(bitmap, targetW, targetH);
+        // Make the Bitmap mutable
         Bitmap mutableBitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true);
+        // Create a Canvas with including the Bitmap
         canvas = new Canvas(mutableBitmap);
+        // Define the Paint object with specific features
         paint = new Paint();
         paint.setColor(Color.BLACK);
-        paint.setStrokeWidth(10);
+        paint.setStrokeWidth(5 / 2);
+        paint.setTextSize(16);
+        // We insert the Bitmap on the ImageView object
         mImageView.setImageBitmap(mutableBitmap);
         mImageView.setOnTouchListener(this);
     }
@@ -178,18 +178,27 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
     public boolean onTouch(View v, MotionEvent event) {
         int action = event.getAction();
         switch (action) {
+            // A pressed gesture has started, the motion contains the initial starting location.
             case MotionEvent.ACTION_DOWN:
+                //Getting the coordinates of the line's bottom
                 downx = event.getX();
                 downy = event.getY();
                 break;
-            case MotionEvent.ACTION_MOVE:
-                break;
+
+            // A pressed gesture has finished, the motion contains the final release location.
             case MotionEvent.ACTION_UP:
+                //Getting the coordinates of the line's top
                 upx = event.getX();
                 upy = event.getY();
+                //We draw the line thanks to the coordinates and the Paint object
                 canvas.drawLine(downx, downy, upx, upy, paint);
-                fillArrow(canvas,downx,downy,upx,upy);
+                // We draw double-ended arrows at each end of the line
+                fillArrow(canvas, downx, downy, upx, upy);
+                // We add the dimension of the line
+                insertText(downx, downy, upx, upy, paint);
                 mImageView.invalidate();
+                break;
+            case MotionEvent.ACTION_MOVE:
                 break;
             case MotionEvent.ACTION_CANCEL:
                 break;
@@ -199,34 +208,34 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
         return true;
     }
 
-    private void fillArrow(Canvas canvas, float x0, float y0, float x1, float y1) {
+    public void fillArrow(Canvas canvas, float x0, float y0, float x1, float y1) {
 
         paint.setStyle(Paint.Style.FILL);
 
         float deltaX = x1 - x0;
         float deltaY = y1 - y0;
         double distance = Math.sqrt((deltaX * deltaX) + (deltaY * deltaY));
-        float frac = (float) (1 / (distance / 30));
+        float frac = (float) (1 / (distance / 15));
 
-        float point_x_1 = x0 + (float) ((1 - frac) * deltaX + frac * deltaY);
-        float point_y_1 = y0 + (float) ((1 - frac) * deltaY - frac * deltaX);
+        float point_x_1 = x0 + ((1 - frac) * deltaX + frac * deltaY);
+        float point_y_1 = y0 + ((1 - frac) * deltaY - frac * deltaX);
 
         float point_x_2 = x1;
         float point_y_2 = y1;
 
-        float point_x_3 = x0 + (float) ((1 - frac) * deltaX - frac * deltaY);
-        float point_y_3 = y0 + (float) ((1 - frac) * deltaY + frac * deltaX);
+        float point_x_3 = x0 + ((1 - frac) * deltaX - frac * deltaY);
+        float point_y_3 = y0 + ((1 - frac) * deltaY + frac * deltaX);
 
-        float x_4 = x0 - deltaX + (float) ((1 - frac) * deltaX + frac * deltaY);
-        float y_4 = y0 - deltaY + (float) ((1 - frac) * deltaY - frac * deltaX);
+        float x_4 = x0 - deltaX + ((1 - frac) * deltaX + frac * deltaY);
+        float y_4 = y0 - deltaY + ((1 - frac) * deltaY - frac * deltaX);
         float point_x_4 = x0 + (x0 - x_4);
         float point_y_4 = y0 + (y0 - y_4);
 
         float point_x_5 = x0;
         float point_y_5 = y0;
 
-        float x_6 = x0 - deltaX + (float) ((1 - frac) * deltaX - frac * deltaY);
-        float y_6 = y0 - deltaY + (float) ((1 - frac) * deltaY + frac * deltaX);
+        float x_6 = x0 - deltaX + ((1 - frac) * deltaX - frac * deltaY);
+        float y_6 = y0 - deltaY + ((1 - frac) * deltaY + frac * deltaX);
         float point_x_6 = x0 + (x0 - x_6);
         float point_y_6 = y0 + (y0 - y_6);
 
@@ -235,17 +244,46 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
         path.setFillType(Path.FillType.EVEN_ODD);
         path2.setFillType(Path.FillType.EVEN_ODD);
 
+        // put the borders of the arrows
         path.moveTo(point_x_1, point_y_1);
         path.lineTo(point_x_2, point_y_2);
         path.lineTo(point_x_3, point_y_3);
         path.close();
 
-        path2.moveTo(point_x_6,point_y_6);
-        path2.lineTo(point_x_5,point_y_5);
+        path2.moveTo(point_x_6, point_y_6);
+        path2.lineTo(point_x_5, point_y_5);
         path2.lineTo(point_x_4, point_y_4);
         path2.close();
 
+        // draw the arrow on the top of the line
         canvas.drawPath(path, paint);
+        // draw the arrow on the bottom of the line
         canvas.drawPath(path2, paint);
+    }
+
+    public void insertText(float startX, float startY, float stopX, float stopY, Paint paint) {
+        float deltaX = stopX - startX;
+        float deltaY = stopY - startY;
+        float x = startX + deltaX / 2;
+        float y = startY + deltaY / 2;
+        // Getting the dimension from the EditText object
+        distanceLine = editText.getText().toString();
+        // Draw the text on the coordinates taken
+        canvas.drawText(distanceLine + "cm", x, y, paint);
+    }
+
+    public Bitmap getResizedBitmap(Bitmap bm, int newWidth, int newHeight) {
+        int width = bm.getWidth();
+        int height = bm.getHeight();
+        // get the ratios between the ImageView dimensions and the Bitmap's
+        float scaleWidth = ((float) newWidth) / width;
+        float scaleHeight = ((float) newHeight) / height;
+        // create a matrix
+        Matrix matrix = new Matrix();
+        // scale the matrix accroding to the widht and height scaled
+        matrix.postScale(scaleWidth, scaleHeight);
+        //resized the Bitmap wanted
+        Bitmap resizedBitmap = Bitmap.createBitmap(bm, 0, 0, width, height, matrix, false);
+        return resizedBitmap;
     }
 }
